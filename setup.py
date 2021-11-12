d2ac9b092900991ec3997aab455a63200989d0c4
RODINcode <70000245+rodincode@users.noreply.github.com>
2021-11-12 16:35:58 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     long_description="A framework for detecting, highlighting and correcting grammatical errors on natural language text",
     url="https://github.com/PrithivirajDamodaran/Gramformer.git",
     packages=setuptools.find_packages(),
-    install_requires=['transformers', 'sentencepiece', 'python-Levenshtein', 'fuzzywuzzy',  'tokenizers', 'fsspec', 'lm-scorer', 'errant'],
+    install_requires=['transformers', 'sentencepiece', 'python-Levenshtein', 'fuzzywuzzy', 'fsspec', 'lm-scorer', 'errant'],
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "License :: Apache 2.0",
